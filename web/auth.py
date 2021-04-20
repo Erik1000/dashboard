37ca1b6accf74709c2380298b6f514ad520aecbe
Erik Tesar <erik@erik-tesar.com>
2021-04-20 14:29:57 +0200

    add comments and add own error page

--- web/auth.py
+++ web/auth.py
@@ -13,16 +13,20 @@ from . import config, main
 from .db.schema import User
 
 router = APIRouter()
+
+# the cookie used to store the session token
 cookie_sec = APIKeyCookie(name="session")
 
 ph = PasswordHasher()
 
 
+# on get render the html page
 @router.get("/login", response_class=HTMLResponse)
 async def view_login_page(request: Request):
     return main.templates.TemplateResponse("login_form.html", {"request": request})
 
 
+# on post try to log the user in
 @router.post("/login", response_class=HTMLResponse)
 async def login(
     request: Request, email: EmailStr = Form(...), password: SecretStr = Form(...)
@@ -30,28 +34,43 @@ async def login(
     successful = False
     reason = None
     token = None
+
+    # search for a user model in the database. If there's no, return None
     if (
         user := await User.query.where(User.user_email == email.lower()).gino.first()
     ) is not None:
+
+        # verify the password hash from the database against the password in the request
         try:
             if ph.verify(user.user_password_hash, password.get_secret_value()):
+                # set to True since the password is correct
                 successful = True
+
+                # check if the password needs a rehash (e.g. because stronger hashing
+                # options are used)
+                # This is only possible on login because the client sends the password.
                 if ph.check_needs_rehash(user.user_password_hash):
+                    # update the new password hash in the database
                     await user.update(
                         user_password_hash=ph.hash(password.get_secret_value())
                     )
+
+                # create a session token. Sessions are only validated by their signature
                 token = jwt.encode(
                     {"sub": str(user.user_uuid)},
                     key=config.SESSION_SECRET.get_secret_value(),
                     algorithm="HS256",
                 )
         except VerifyMismatchError:
+            # the password hashes don't match -> wrong password
             successful = False
             reason = "Wrong password."
     else:
+        # the user is not in the database and therefore doesn't exist -> unknown
         successful = False
         reason = "Unknown email."
 
+    # render the response page. Look at the template file to see what is displayed when
     response = main.templates.TemplateResponse(
         "login_response.html",
         {
@@ -61,16 +80,22 @@ async def login(
             "successful": successful,
         },
     )
+
     if token is not None:
+        # at the end append the session token to the respond as cookie
         response.set_cookie("session", token)
+
+    # return the rendered response
     return response
 
 
+# on get render the register form
 @router.get("/register", response_class=HTMLResponse)
 async def view_registration_page(request: Request):
     return main.templates.TemplateResponse("register_form.html", {"request": request})
 
 
+# register a new user with their email address and password
 @router.post("/register", response_class=HTMLResponse)
 async def register(
     request: Request,
@@ -80,22 +105,30 @@ async def register(
 ):
     successful = False
     reason = None
+
+    # check if the two passwords are the same
     if password == password_confirm:
+        # check if the user already exists in the database
         if (
             await User.query.where(User.user_email == email.lower()).gino.first()
             is not None
         ):
+            # if so set the corresponding reason
             reason = "Already registered."
         else:
+            # if the user is not already in the database, insert their
             await User.create(
                 user_uuid=uuid.uuid4(),
                 user_email=email.lower(),
                 user_password_hash=ph.hash(password.get_secret_value()),
             )
+            # set successful to True after the insert was successful
             successful = True
     else:
+        # The passwords are not the same. Set the corresponding reason.
         reason = "Passwords not equal."
 
+    # render the response template. See the template file to know what is displayed when
     return main.templates.TemplateResponse(
         "register_response.html",
         {
@@ -107,13 +140,20 @@ async def register(
     )
 
 
+# Function used with Depends on sites that require authentication.
+# returns the UUID of the user.
 async def get_current_user(session: str = Depends(cookie_sec)) -> uuid.UUID:
     try:
+        # try to decode the session token and check the signature
         payload = jwt.decode(
             session, config.SESSION_SECRET.get_secret_value(), algorithms="HS256"
         )
+
+        # return the UUID in the 'sub' field
         return payload["sub"]
     except exceptions.PyJWTError:
+        # If the session token is invalid in any way, raise an HTTPException handled by
+        # fastapi
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN, detail="Invalid session!"
         )
--- web/main.py
+++ web/main.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.config import Config
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from . import auth
 from .auth import get_current_user
@@ -14,6 +15,7 @@ templates = Jinja2Templates(directory="web/templates")
 app.include_router(auth.router, prefix="/auth")
 
 
+# render some example page if authenticated
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request, uuid=Depends(get_current_user)):
     return templates.TemplateResponse("index.html", {"request": request, "uuid": uuid})
@@ -21,9 +23,19 @@ async def root(request: Request, uuid=Depends(get_current_user)):
 
 @app.on_event("startup")
 async def startup():
+    # connect to the database
     await connection.init_connection()
 
 
 @app.on_event("shutdown")
 async def shutdown():
+    # disconnect from the database
     await connection.close_connection()
+
+
+# add custom exception handler for HTTPException so it doesn't respond with json.
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, e: StarletteHTTPException):
+    return templates.TemplateResponse(
+        "error.html", {"request": request, "code": e.status_code, "detail": e.detail}
+    )
